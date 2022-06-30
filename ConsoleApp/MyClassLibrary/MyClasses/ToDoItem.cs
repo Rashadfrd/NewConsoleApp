41d951700baae2ada4c56adab8f74f1999f73f8b
Rashadfrd <rashadnf@code.edu.az>
2022-06-30 14:18:50 +0400

    just started

---
+++ ConsoleApp/MyClassLibrary/MyClasses/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyClassLibrary.MyClasses
+{
+    public class ToDoItem
+    {
+        public ToDoItem()
+        {
+            _totalCount++;
+            this._no = _totalCount;
+        }
+
+        public static int _totalCount { get; set; }
+        public int _no { get; }
+        public string Title;
+        public string Description;
+        public DateTime Deadline;
+        public DateTime StatusChangedAt;
+    }
+}
